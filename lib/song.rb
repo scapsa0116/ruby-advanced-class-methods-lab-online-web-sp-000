9aa2773541f8425df56475a173f888c65fd9404d
Stela Capsa <stela.capsa@gmail.com>
2019-12-31 12:18:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,37 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
+def  initialize(name) 
+  @name = name
+  @@all << self
+  save
+end
+
+  
   def self.all
     @@all
-  end
+   end
 
+  
   def save
     self.class.all << self
   end
+   
+   
+   def self.create
+     self.class.all << @name
+   end 
+   
+   
+ 
+ def new_by_name(name)
+   @@all.find {|person| person.name == name}
+   peson
+ end
 
+
+def self.destroy_all
+  @@all = []
+end
 end
