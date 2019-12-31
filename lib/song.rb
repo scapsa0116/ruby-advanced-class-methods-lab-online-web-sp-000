fddda80d0dc51ebc1c7fe1756a89796a662dcc44
Stela Capsa <stela.capsa@gmail.com>
2019-12-31 12:51:13 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,71 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
   def self.all
     @@all
-  end
+   end
 
+  
   def save
     self.class.all << self
   end
+   
+  
+  def self.create
+    song = self.new
+    @@all << song  #saves it
+    song   #returns the new song
+  end
+ 
+ def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song  #saves the song
+    song  # returns the song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|songs| songs.name == name}
+  end
+
+
+ def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
+       #if first statement is not true than will move to the second 
+  end
+  
+  
+  def self.alphabetical()
+    @@all.sort_by{|songs| songs.name}
+  end
+  
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
+  
+
+def self.destroy_all
+  @@all = []
+end
 
 end
+
