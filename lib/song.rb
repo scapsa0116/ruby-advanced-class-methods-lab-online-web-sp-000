a5f67986bc9280290eb385e02770fb745413f88b
Stela Capsa <stela.capsa@gmail.com>
2019-12-31 12:42:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,16 @@ class Song
   end
 
 
+ def self.find_or_create_by_name(name)
+      #if self.find_by_name(name) == nil 
+        #self.create_by_name(name)
+      #else
+        #self.find_by_name(name)
+      #end
+      self.find_by_name(name) || self.create_by_name(name)
+      #the above statement is saying do this(if it is true) or that (if the first thing is not true and the second thing is true)
+  end
+
 def self.destroy_all
   @@all = []
 end
